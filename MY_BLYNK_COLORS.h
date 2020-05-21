de072409649d5be970661ff07e2fa60dcf92ca88
thorathome <thorathome@aol.com>
2020-05-21 10:06:07 -0400

    Add files via upload

---
+++ MY_BLYNK_COLORS.h
@@ -0,0 +1,11 @@
+// BLYNK COLOR PALETTE SAMPLE
+#define BLYNK_RED              "#D3435C"
+#define BLYNK_GREEN            "#23C48E"
+#define BLYNK_BLUE             "#04C0F8"
+#define BLYNK_DK_BLUE          "#5F7CD8"
+#define BLYNK_WHITE            "#FFFFFF"
+  
+#define BLYNK_TEAL             "#00AE97"
+#define BLYNK_REALLY_RED       "#F90000"
+#define BLYNK_PLEASANT_GREEN   "#009140"
+#define BLYNK_DK_GREY_BACKGRND "#212266"
