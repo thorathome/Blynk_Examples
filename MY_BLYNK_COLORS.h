4b524a23b1522ac5c82bbf15f6297f643760911f
Thor Johnson <thorathome@aol.com>
2020-07-06 17:10:27 -0400

    Update MY_BLYNK_COLORS.h

--- MY_BLYNK_COLORS.h
+++ MY_BLYNK_COLORS.h
@@ -1,14 +1,17 @@
-// #include "MY_BLYNK_COLORS.h" 
 // BLYNK COLOR PALETTE SAMPLE
-#define BLYNK_GREEN             "#23C48E"
-#define BLYNK_BLUE              "#04C0F8"
-#define BLYNK_YELLOW            "#ED9D00"
-#define BLYNK_RED               "#D3435C"
-#define BLYNK_DARK_BLUE         "#5F7CD8"
+//#include "MY_BLYNK_COLORS.h"       // #defines a bunch of handy Blynk colors
 
-#define BLYNK_WHITE             "#FFFFFF"
+#define BLYNK_GREEN            "#23C48E"
+#define BLYNK_BLUE             "#04C0F8"
+#define BLYNK_YELLOW           "#ED9D00"
+#define BLYNK_RED              "#D3435C"
+#define BLYNK_DARK_BLUE        "#5F7CD8"
+
+#define BLYNK_WHITE            "#FFFFFF"
+#define BLYNK_GREY             "#878889"
   
-#define BLYNK_TEAL              "#00AE97"
-#define BLYNK_REALLY_RED        "#F90000"
-#define BLYNK_PLEASANT_GREEN    "#009140"
-#define BLYNK_DK_GREY_BACKGRND  "#212266"
+#define BLYNK_TEAL             "#00AE97"
+#define BLYNK_REALLY_RED       "#F90000"
+#define BLYNK_PLEASANT_GREEN   "#009140"
+#define BLYNK_DK_GREY_BACKGRND "#212266"
+
