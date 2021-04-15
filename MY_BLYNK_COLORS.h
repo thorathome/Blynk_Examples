595807e84b059c669b5557074e62bbd8059ec7b5
Thor Johnson <2821513+thorathome@users.noreply.github.com>
2021-04-15 17:47:06 -0400

    Added more colors - Blynk app button colors too
    Added the colors of the default Blynk app buttons

--- MY_BLYNK_COLORS.h
+++ MY_BLYNK_COLORS.h
@@ -1,12 +1,22 @@
 // BLYNK COLOR PALETTE SAMPLE
 //#include "MY_BLYNK_COLORS.h"       // #defines a bunch of handy Blynk colors
 
+// "Standard" Blynk colors from somewhere?
 #define BLYNK_GREEN            "#23C48E"
 #define BLYNK_BLUE             "#04C0F8"
 #define BLYNK_YELLOW           "#ED9D00"
 #define BLYNK_RED              "#D3435C"
 #define BLYNK_DARK_BLUE        "#5F7CD8"
 
+// These are the colors in the Blynk App for LEDs, buttons, type...
+#define BLYNK_BUTTON_GREEN     "#5CBE8F"
+#define BLYNK_BUTTON_DKBLUE    "#6379CF"
+#define BLYNK_BUTTON_LTBLUE    "#55B9EF"
+#define BLYNK_BUTTON_ORANGE    "#DD9E36"
+#define BLYNK_BUTTON_RED       "#C04D5D"
+#define BLYNK_BUTTON_WHITE     "#FAFAFB"
+
+// More handy colors
 #define BLYNK_WHITE            "#FFFFFF"
 #define BLYNK_GREY             "#878889"
   
@@ -14,4 +24,7 @@
 #define BLYNK_REALLY_RED       "#F90000"
 #define BLYNK_PLEASANT_GREEN   "#009140"
 #define BLYNK_DK_GREY_BACKGRND "#212266"
+
+#define BLYNK_VIOLET           "#9900DF"
+#define BLYNK_ORANGE           "#FE7F01"
 
